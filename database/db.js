447ed9220547cdb72e5aa82b7dfd7b63c3c3098b
Vince Salenga <vince.salenga@gmail.com>
2025-10-06 01:28:57 +0800

    Initialized database - Infinityfree

--- database/db.js
+++ database/db.js
@@ -1,13 +1,27 @@
-import mysql from "mysql2/promise"
+// import mysql from "mysql2/promise"
+
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "heroes",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// })
+
+// export default pool
+
+import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "heroes",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-})
+});
 
-export default pool
+export default pool;
